3b263ecdc8b8ada7d70032e95ee569eabf6905b0
345784078 <74387404+345784078@users.noreply.github.com>
2020-12-14 16:14:03 +0800

    Update jd_updateBeanHome.js

--- jd_updateBeanHome.js
+++ jd_updateBeanHome.js
@@ -78,7 +78,7 @@ function showMsg() {
 }
 
 async function writeFile() {
-  await fs.writeFileSync('jd_updateBeanHome.json', JSON.stringify($.codeInfo));
+  await fs.writeFileSync('https://github.com/345784078/updateTeam/blob/master/jd_updateBeanHome.json', JSON.stringify($.codeInfo));
   console.log(`文件写入成功,inviteCode已经替换`);
 }
 
